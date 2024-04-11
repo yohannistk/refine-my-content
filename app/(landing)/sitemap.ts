adfd6dcabf5b635ad4802c715c5afe0ed981abe7
john <onlinejobs2116@gmail.com>
2024-04-11 19:54:36 +0300

    sitemap added

---
+++ app/(landing)/sitemap.ts
@@ -0,0 +1,56 @@
+import { MetadataRoute } from "next";
+import {} from "@/helpers/index";
+import { getURL } from "@/utils/helpers";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: getURL("/"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: getURL("/paraphraser"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: getURL("/summarizer"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: getURL("/image-to-text"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: getURL("/terms-and-conditions"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.6,
+    },
+    {
+      url: getURL("/privacy-policy"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.6,
+    },
+    {
+      url: getURL("/contact"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.6,
+    },
+    {
+      url: getURL("/more"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.6,
+    },
+  ];
+}
